7d3c073e616eb1dc87a12247a1238ced4d8fea66
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2024-12-25 13:09:58 +0600

    brute force solutions

---
+++ ArrayList/Medium/ContainerWithMostWater.java
@@ -0,0 +1,33 @@
+//brute force O(n^2)
+
+import java.util.*;
+public class ContainerWithMostWater{
+
+    public static int maxxWater(ArrayList<Integer> height){
+        int maxWater = 0;
+        for(int i=0; i <height.size();i++){
+            for(int j= i+1;j<height.size();j++){
+               int width =  j-i;
+               int h = Math.min(height.get(i),height.get(j));
+               int water = width * h;
+            maxWater = Math.max(maxWater,water);
+            }
+        }
+        return maxWater;
+    }
+    public static void main(String args[]){
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+        System.out.println(maxxWater(height));
+
+    }
+
+}
